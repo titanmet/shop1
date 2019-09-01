92d80bf13dc304f10811bc7a6ec186facb1e855a
titanmet <titanmet@mail.ru>
2019-09-01 14:29:33 +0300

    Added social login controller

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -2,10 +2,16 @@
 
 $config = [
     'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
-        ],
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'vk' => [
+                    'class' => 'yii\authclient\clients\VKontakte',
+                    'clientId' => '',
+                    'clientSecret' => '',
+                ],
+            ],
+        ]
     ],
 ];
 
