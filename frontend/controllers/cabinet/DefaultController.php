0c9b1dd94052564594d208677d6b46b6edc09523
titanmet <titanmet@mail.ru>
2019-09-01 14:40:15 +0300

    Added cabinet controller

---
+++ frontend/controllers/cabinet/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+namespace frontend\controllers\cabinet;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+class DefaultController extends Controller
+{
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+    /**
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
