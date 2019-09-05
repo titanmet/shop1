e03ec71aa594ab130b787f65aa2aa18b478542a3
titanmet <titanmet@mail.ru>
2019-09-05 10:42:09 +0300

    Added status label

--- backend/views/user/view.php
+++ backend/views/user/view.php
@@ -1,8 +1,8 @@
 <?php
+use shop\helpers\UserHelper;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
-/* @var $this yii\web\View */
-/* @var $model shop\entities\User */
+
 $this->title = $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
@@ -30,7 +30,11 @@ $this->params['breadcrumbs'][] = $this->title;
                         'id',
                         'username',
                     'email:email',
-                    'status',
+                    [
+                        'attribute' => 'status',
+                        'value' => UserHelper::statusLabel($model->status),
+                        'format' => 'raw',
+                    ],
                     'created_at:datetime',
                     'updated_at:datetime',
                    ],
