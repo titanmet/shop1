0cebbeaa277fdb9a9696b1a32b6c7537ce98d852
titanmet <titanmet@mail.ru>
2019-09-05 22:40:52 +0300

    Removed unused rules

--- shop/entities/User/User.php
+++ shop/entities/User/User.php
@@ -9,21 +9,7 @@ use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
 
-/**
- * User model
- *
- * @property integer $id
- * @property string $username
- * @property string $password_hash
- * @property string $password_reset_token
- * @property string $verification_token
- * @property string $email
- * @property string $auth_key
- * @property integer $status
- * @property integer $created_at
- * @property integer $updated_at
- * @property string $password write-only password
- */
+
 class User extends ActiveRecord implements IdentityInterface
 {
     use InstantiateTrait;
@@ -134,15 +120,6 @@ class User extends ActiveRecord implements IdentityInterface
         ];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_WAIT]],
-        ];
-    }
 
     /**
      * {@inheritdoc}
