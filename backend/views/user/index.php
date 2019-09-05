46f12aba0297fbd390f1b862de5bcc74177a5b04
titanmet <titanmet@mail.ru>
2019-09-05 10:49:26 +0300

    Removed unused h1

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -4,15 +4,12 @@ use shop\helpers\UserHelper;
 use yii\grid\ActionColumn;
 use yii\helpers\Html;
 use yii\grid\GridView;
-/* @var $this yii\web\View */
-/* @var $searchModel backend\forms\UserSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+
 $this->title = 'Users';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
