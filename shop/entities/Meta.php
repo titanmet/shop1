300f76e2e0b815790dd341a7331e92606c00acfc
titanmet <titanmet@mail.ru>
2019-09-08 16:04:11 +0300

    Added brand

---
+++ shop/entities/Meta.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace shop\entities;
+class Meta
+{
+    public $title;
+    public $description;
+    public $keywords;
+
+    public function __construct($title, $description, $keywords)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->keywords = $keywords;
+    }
+} 
