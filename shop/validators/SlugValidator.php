e13d56b68b986f946bf6b13c4ecfc100a85ad266
titanmet <titanmet@mail.ru>
2019-09-10 17:38:12 +0300

    Extracted slug validator

---
+++ shop/validators/SlugValidator.php
@@ -0,0 +1,8 @@
+<?php
+namespace shop\validators;
+use yii\validators\RegularExpressionValidator;
+class SlugValidator extends RegularExpressionValidator
+{
+    public $pattern = '#^[a-z0-9_-]*$#s';
+    public $message = 'Only [a-z0-9_-] symbols are allowed.';
+}
