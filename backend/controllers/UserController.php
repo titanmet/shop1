fdae8de6c40201d807112beb55be73ba15d82aed
titanmet <titanmet@mail.ru>
2019-09-15 13:48:35 +0300

    Added product creating

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace backend\controllers;
+
 use shop\forms\manage\User\UserCreateForm;
 use shop\forms\manage\User\UserEditForm;
 use shop\services\manage\UserManageService;
@@ -9,114 +11,121 @@ use backend\forms\UserSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+
 /**
-* UserController implements the CRUD actions for User model.
-*/
+ * UserController implements the CRUD actions for User model.
+ */
 class UserController extends Controller
 {
     private $service;
+
     public function __construct($id, $module, UserManageService $service, $config = [])
     {
         parent::__construct($id, $module, $config);
         $this->service = $service;
     }
-/**
-* @inheritdoc
-*/
-public function behaviors()
-{
-return [
-'verbs' => [
-'class' => VerbFilter::className(),
-'actions' => [
-'delete' => ['POST'],
-],
-],
-];
-}
-/**
-* Lists all User models.
-* @return mixed
-*/
-public function actionIndex()
-{
-$searchModel = new UserSearch();
-$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-return $this->render('index', [
-'searchModel' => $searchModel,
-'dataProvider' => $dataProvider,
-]);
-}
-/**
-* Displays a single User model.
-* @param integer $id
-* @return mixed
-*/
-public function actionView($id)
-{
-return $this->render('view', [
-'model' => $this->findModel($id),
-]);
-}
-/**
-* Creates a new User model.
-* If creation is successful, the browser will be redirected to the 'view' page.
-* @return mixed
-*/
-public function actionCreate()
-{
-    $form = new UserCreateForm();
-    if ($form->load(Yii::$app->request->post()) && $form->validate()) {
-        try {
-            $user = $this->service->create($form);
-            return $this->redirect(['view', 'id' => $user->id]);
-  } catch (\DomainException $e) {
-            Yii::$app->errorHandler->logException($e);
-            Yii::$app->session->setFlash('error', $e->getMessage());
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all User models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new UserSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single User model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new User model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $form = new UserCreateForm();
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $user = $this->service->create($form);
+                return $this->redirect(['view', 'id' => $user->id]);
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+        return $this->render('create', [
+            'model' => $form,
+        ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $user = $this->findModel($id);
+        $form = new UserEditForm($user);
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $this->service->edit($user->id, $form);
+                return $this->redirect(['view', 'id' => $user->id]);
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+        return $this->render('update', [
+            'model' => $form,
+            'user' => $user,
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        $this->service->remove($id);
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the User model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return User the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = User::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
-    return $this->render('create', [
-        'model' => $form,
-    ]);
-}
-
-public function actionUpdate($id)
-{
-    $user = $this->findModel($id);
-    $form = new UserEditForm($user);
-    if ($form->load(Yii::$app->request->post()) && $form->validate()) {
-        try {
-            $this->service->edit($user->id, $form);
-            return $this->redirect(['view', 'id' => $user->id]);
-        } catch (\DomainException $e) {
-            Yii::$app->errorHandler->logException($e);
-            Yii::$app->session->setFlash('error', $e->getMessage());
-        }
-    }
-    return $this->render('update', [
-        'model' => $form,
-        'user' => $user,
-    ]);
-}
-
-public function actionDelete($id)
-{
-    $this->service->remove($id);
-    return $this->redirect(['index']);
-}
-/**
-* Finds the User model based on its primary key value.
-* If the model is not found, a 404 HTTP exception will be thrown.
-* @param integer $id
-* @return User the loaded model
-* @throws NotFoundHttpException if the model cannot be found
-*/
-protected function findModel($id)
-{
-if (($model = User::findOne($id)) !== null) {
-return $model;
-} else {
-throw new NotFoundHttpException('The requested page does not exist.');
-}
-}
 }
